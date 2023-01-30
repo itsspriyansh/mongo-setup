5dddd1d15609480f62fe9059999e5582fa5cdb74
itsspriyansh <priyanshkp01@gmail.com>
2023-01-30 14:19:02 +0530

    finished

--- db.js
+++ db.js
@@ -2,9 +2,12 @@ const { MongoClient } = require("mongodb")
 
 let dbConnection
 
+const uri = "mongodb+srv://itsspiryansh:tic9wdKMAaSVF7r6@cluster0.mbi5xxx.mongodb.net/test"
+
+
 module.exports = {
     connectToDb : (cb) => {
-        MongoClient.connect("mongodb://localhost:27017/bookstore")
+        MongoClient.connect(uri)
         .then (client => {
             dbConnection = client.db()
             return cb()
